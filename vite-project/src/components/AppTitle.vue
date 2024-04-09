4902249646bb812556bff4b7dbea3bb484ea17fa
Davide-Morellato <morellato.davide@yahoo.it>
2024-04-09 15:58:24 +0200

    ADD: AppTitle + AppHero

---
+++ vite-project/src/components/AppTitle.vue
@@ -0,0 +1,23 @@
+<script>
+    export default{
+        data(){
+            return{
+                message: 'La mia prima volta con VITE....'
+            }
+        }
+    }
+</script>
+
+<template>
+    <div>
+        <h1 class="center">
+            {{ message }}
+        </h1>
+    </div>
+</template>
+
+<style scoped>
+    .center{
+        text-align: center;
+    }
+</style>
