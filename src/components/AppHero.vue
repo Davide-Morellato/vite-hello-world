42adb50e8f23587f11b4d2cb70d56b236d48f2af
Davide-Morellato <morellato.davide@yahoo.it>
2024-04-09 18:29:00 +0200

    ADD components: AppTitle AND AppHero

---
+++ src/components/AppHero.vue
@@ -0,0 +1,28 @@
+<script>
+    export default {
+        data() {
+            return {
+                finalMessage: '....al Limite!'
+            }
+        }
+    }
+</script>
+
+<template>
+    <div>
+        <img src="https://cdn.blogo.it/XUe6DS0DZX_LMqHhUTPcqyu3h6g=/1280x720/smart/https://www.blogo.it/app/uploads/sites/2/2020/03/joe-my-600-lb-life-2.jpg" alt="">
+        <h1 class="center">
+            {{ finalMessage }}
+        </h1>
+    </div>
+</template>
+
+<style>
+    .center{
+        text-align: center;
+    }
+
+    img{
+        width: 60%;
+    }
+</style>
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,21 @@
+<script>
+    export default {
+        data() {
+            return {
+                title: 'La mia prima volta con VITE....'
+            }
+        }
+    }
+</script>
+
+<template>
+    <h1 class="center">
+        {{ title }}
+    </h1>
+</template>
+
+<style>
+    .center{
+        text-align: center;
+    }
+</style>
