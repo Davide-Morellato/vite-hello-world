a99238b5c5c3759fb689de0d39da475da0c137be
Davide-Morellato <morellato.davide@yahoo.it>
2024-04-09 17:21:59 +0200

    Change script in App.vue: delete setup; add export default {components:{...}}

--- vite-project/src/App.vue
+++ vite-project/src/App.vue
@@ -1,6 +1,13 @@
 <script>
   import AppTitle from './components/AppTitle.vue'
   import AppHero from './components/AppHero.vue'
+
+  export default {
+    components:{
+      AppTitle,
+      AppHero
+    }
+  }
 </script>
 
 <template>
